4de03e3bbcee7081778e49f7622f79c83d74fe85
ravensramen <153202736+ravensramen@users.noreply.github.com>
2024-03-25 20:37:38 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "header.h"
+
+int main() {
+
+	srand((time(NULL))); //allows random boat placement and shots by the computer player
+
+	printf("Welcome to Battleship!\n\n"); //Welcome message upon first launch
+
+	char player_1_gameboard[NUM_ROWS][NUM_COLS] = { {'\0'} }; //player's gameboard
+	char player_2_gameboard[NUM_ROWS][NUM_COLS] = { {'\0'} }; //computer's gameboard
+	int choice = 0; //initialize choice variable
+
+	do {
+		menu_choice(&choice); //function that displays menu and prompts for user choice, menu repeats each round
+
+		switch (choice) { //switch statement runs program based on user's choice
+		case 1:
+			display_rules(); //function to display rules
+			break;
+		case 2:
+			battleship_gameplay(player_1_gameboard, player_2_gameboard); //main gameplay function
+			break;
+		case 3:
+			printf("Goodbye..."); //goodbye message
+			break;
+		}
+
+	} while (choice != 3); //as long as the user doesn't choose to exit, menu repeats 
+
+	return 0; 
+}
